b3a143f1eb5e6a1b6b05120dbbb81ce726293324
Tamás Varjas <tamas.varjas00@gmail.com>
2018-02-20 12:21:20 +0100

    Boards and cards can be retrieved from DB

--- queries.py
+++ queries.py
@@ -1,1 +1,9 @@
-from data import data_manager
+import data_manager
+
+
+def get_boards():
+    return data_manager.execute_select('SELECT * FROM boards;')
+
+
+def get_cards():
+    return data_manager.execute_select('SELECT * FROM cards;')
