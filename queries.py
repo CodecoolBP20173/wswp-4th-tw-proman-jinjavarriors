9d8369b9a566a9f88b3a4f2a5b53d24d2e6af3ad
Ferenc Bolla <bollaferenc@gmail.com>
2018-02-22 10:48:15 +0100

    Boards are shown by creation date order

--- queries.py
+++ queries.py
@@ -3,7 +3,8 @@ import data_manager
 
 def get_boards(user_id):
     return data_manager.execute_select('''SELECT * FROM boards
-                                        WHERE user_id = %(user_id)s;
+                                        WHERE user_id = %(user_id)s
+                                        ORDER BY creation_time ASC;
                                        ''',
                                        {'user_id': user_id})
 
